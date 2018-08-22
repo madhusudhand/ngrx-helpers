5a627fb2c8a0e416ae97d251d3229aac4284873f
madhusudhand <madhusudhan.dollu@gmail.com>
2018-08-22 12:06:19 +0530

    create angular 6 project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { ActionReducerMap, StoreModule } from '@ngrx/store';
+import * as fromRouter from '@ngrx/router-store';
+
+const rootReducers: ActionReducerMap<any> = {
+  router: fromRouter.routerReducer,
+  // logger,
+};
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
